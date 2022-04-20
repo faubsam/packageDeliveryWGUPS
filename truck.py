0d6aca6db6c516684cdeab5208f572118f293032
faubsam <faubertsamuel@gmail.com>
2022-04-20 18:30:44 -0400

    truck

--- truck.py
+++ truck.py
@@ -1,2 +1,12 @@
 class Truck:
-    pass
+    max_packages = 16
+    speed = 18
+    truck_packages = []
+
+    def __init__(self, max_packages=max_packages, speed=speed):
+        self.max_packages = max_packages
+        self.speed = speed
+
+    
+    def add_package(self, package):
+        self.truck_packages.append(package)
