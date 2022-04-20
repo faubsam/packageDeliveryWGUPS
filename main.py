c9aea12941d6f24e6c217ad229e119e0929c3b8b
faubsam <faubertsamuel@gmail.com>
2022-04-20 18:29:01 -0400

    refactored classes, added csv reader for 3 files

--- main.py
+++ main.py
@@ -1,9 +1,88 @@
 # Author: Samuel Faubert
 # ID: 001385881
 
-def launch():
-    print("Launching")
+import csv
+from itertools import islice
+import csv_helper
+
+    
+
+
+
+class Truck:
+    max_packages = 16
+    speed = 18
+    truck_packages = []
+
+    def __init__(self, max_packages=max_packages, speed=speed):
+        self.max_packages = max_packages
+        self.speed = speed
+
+    
+    def add_package(self, package):
+        self.truck_packages.append(package)
+
+
+class HashTable:
+    # Constructor for the hash table class which creates a new, blank hash table
+    # Default capacity is 50
+    # This is a chaining hash table, each bucket is initialized with an empty list
+    def __init__(self, initial_capacity=50):
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    # Insert function takes a node as the value to add to the hash table
+    # The bucket to insert is retrieved from the modulo of the size of the hash table
+    # The node gets added to the list in the selected bucket
+    def insert(self, id, package):
+        bucket = hash(id) % len(self.table)
+        new_package = [id, package]
+
+        if self.table[bucket] is None:
+            self.table[bucket] = list([new_package])
+            return True
+        else:
+            for bucket_list in self.table[bucket]:
+                if bucket_list == id:
+                    bucket_list[1] = package
+                    return True
+            self.table[bucket].append(new_package)
+            return True
+
+    # Lookup parameter returns the value in the bucket list for the given key parameter
+    def lookup(self, package):
+        bucket = hash(package) % len(self.table)
+        bucket_list = self.table[bucket]
+        if package in bucket_list:
+            node_index = bucket_list.index(package)
+            return bucket_list[node_index]
+        else:
+            return None
+
+
+    # Remove function takes a key as the parameter and removes it from the bucket list if it is there
+    def delete(self, package):
+        bucket = hash(package) % len(self.table)
+        bucket_list = self.table[bucket]
+        if package in bucket_list:
+            bucket_list.remove(package)
+
+    def print_map(self):
+        for i in range(50):
+            if len(self.table[i]) > 0:
+                
+                print(self.table[i][0][1].address)
+    
 
 
 if __name__ == "__main__":
-    launch()
+    csv_reader = csv_helper.CSV_helper()
+
+
+    packagesTable = HashTable()
+    csv_reader.load_package_data("DSA2/packageDeliveryWGUPS/WGUPS Package File.csv", packagesTable)
+    distance_data = csv_reader.load_distance_data("/home/sam/DSA2/packageDeliveryWGUPS/distances.csv")
+    addresses_data = csv_reader.load_addresses("/home/sam/DSA2/packageDeliveryWGUPS/addresses.csv")
+    
+    
--- package.py
+++ package.py
@@ -1,4 +1,7 @@
-from contextlib import nullcontext
+
+
+
+import csv
 
 
 class Package:
@@ -10,5 +13,31 @@ class Package:
     weight = 0
     delivery_status = ""
 
-    def __init__(self) -> None:
-        pass
+    # Constructor for package object with all required parameters from the requirements
+    def __init__(self, id, address, deadline, city, zip_code, weight, delivery_status="At hub facility"):
+        self.id = id
+        self.address = address
+        self.deadline = deadline
+        self.city = city
+        self.zip_code = zip_code
+        self.weight = weight
+        self.delivery_status = delivery_status
+
+    
+    def loadPackageData(file, hashTable):
+        with open(file) as package_file:
+            packageData = csv.reader(package_file)
+            next(packageData)
+            for package in packageData:
+                p_id = int(package[0])
+                p_address = package[1]
+                p_deadline = package[5]
+                p_city = package[2]
+                p_zip_code = package[4]
+                p_weight = package[6]
+
+                p_package = Package(p_id, p_address, p_deadline, p_city, p_zip_code, p_weight)
+
+                hashTable.insert(p_id, p_package)
+
+
