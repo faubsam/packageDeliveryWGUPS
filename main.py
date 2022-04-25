a69a0f0a559bc70bc2d135c7efa418c355302a7a
faubsam <faubertsamuel@gmail.com>
2022-04-25 16:55:08 -0400

    packages delivered 136.2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # ID: 001385881
 
 import csv
+from datetime import datetime
 from struct import pack
 import csv_helper
 import hashTable
@@ -14,9 +15,7 @@ if __name__ == "__main__":
     print("Starting deliveries for the day...........")
     package_delivery_day.load_truck_packages()
     
-    print(package_delivery_day.truck1.current_packages)
-    print(package_delivery_day.truck2.current_packages)
-    print(package_delivery_day.truck3.current_packages)
+    
     package_delivery_day.deliver_packages(package_delivery_day.truck1)
     package_delivery_day.deliver_packages(package_delivery_day.truck2)
     if package_delivery_day.truck1.current_packages == 0 and package_delivery_day.truck1.current_location == package_delivery_day.addresses_data[0][1]:
